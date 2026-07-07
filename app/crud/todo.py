572f9449e0219118cb1e9f99f8e0d7a320cbe578
owais-js <owaisshabbir3131@gmail.com>
2026-07-07 23:22:04 +0500

    crud folder created and crud functions done

---
+++ app/crud/todo.py
@@ -0,0 +1,52 @@
+from sqlalchemy.orm import Session
+
+from app.models.todo import Todo
+from app.schemas.todo import TodoCreate
+
+
+def create_todo(db: Session, todo: TodoCreate):
+    db_todo = Todo(
+        title=todo.title,
+        description=todo.description
+    )
+
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+
+    return db_todo
+
+
+def get_todos(db: Session):
+    return db.query(Todo).all()
+
+
+def get_todo(db: Session, todo_id: int):
+    return db.query(Todo).filter(Todo.id == todo_id).first()
+
+
+def update_todo(db: Session, todo_id: int, todo: TodoCreate):
+    db_todo = get_todo(db, todo_id)
+
+    if not db_todo:
+        return None
+
+    db_todo.title = todo.title
+    db_todo.description = todo.description
+
+    db.commit()
+    db.refresh(db_todo)
+
+    return db_todo
+
+
+def delete_todo(db: Session, todo_id: int):
+    db_todo = get_todo(db, todo_id)
+
+    if not db_todo:
+        return None
+
+    db.delete(db_todo)
+    db.commit()
+
+    return db_todo
