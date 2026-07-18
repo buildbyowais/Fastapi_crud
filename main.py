a0773e7cbcf3906db24e9808426e187e9ecf867c
owais-js <owaisshabbir3131@gmail.com>
2026-07-18 15:27:50 +0500

    user router added in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.api.todo import router as todo_router
+from app.api.user import router as user_router
 
 app = FastAPI( title="Todo API" )
 
 app.include_router(todo_router)
+app.include_router(user_router)
 
 @app.get("/") 
 def home(): 
